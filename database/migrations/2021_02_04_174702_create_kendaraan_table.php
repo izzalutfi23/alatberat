f96bb2ee94d17cef6c9581bb4a2409a17d9e69dd
izza lutfi <izzalutfi045@gmail.com>
2021-02-13 22:13:11 +0700

    add migration

---
+++ database/migrations/2021_02_04_174702_create_kendaraan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKendaraanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kendaraan', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('kategori_id')->unsigned();
+            $table->char('nama', 255);
+            $table->text('deskripsi');
+            $table->integer('harga')->unsigned();
+            $table->enum('status', ['0', '1']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kendaraan');
+    }
+}
